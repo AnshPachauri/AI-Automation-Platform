9256a4f2bb69814e253ad3300f3b016f7e9bfbb1
Ansh Kumar Pachauri <anshpachauri84@gmail.com>
2026-01-02 07:13:50 +0530

    theme and styling

---
+++ src/features/auth/components/auth-layout.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export const AuthLayout = ({ children } : { children: React.ReactNode }) => {
+    return (
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+            <Link
+            href="/"
+            className="flex items-center gap-2 self-center text-2xl font-extrabold tracking-tight text-foreground"
+            >
+            <span className="text-primary">AKP</span>
+            <span>Automate</span>
+            </Link>
+            {children}
+        </div>
+        </div>
+    )
+}

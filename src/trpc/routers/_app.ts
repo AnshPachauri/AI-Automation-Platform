92a1a15adf18503ac03f7c29053e038d24bc8944
Ansh Kumar Pachauri <anshpachauri84@gmail.com>
2026-01-02 08:43:51 +0530

    inngest setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,19 @@
+import { inngest } from "@/inngest/client";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure
+  getWorkflows: protectedProcedure
     .query(() => {
-        return prisma.user.findMany();
+        return prisma.workflow.findMany();
     }),
+    createWorkflow: protectedProcedure.mutation(async()=>{
+      return  inngest.send({
+        name: "test/hello.world", // should match the event defined in the function
+        data: {
+          name: "Test Workflow"
+        }
+      });
+    })
 });
 
 // export type definition of API
